f04eb8918dbf6566a37da9764cd3e0d9f8b880a4
OMPRATIK <routprateek4@gmail.com>
2024-01-24 21:39:12 +0530

    new pattern

---
+++ pattern/pattern.java
@@ -0,0 +1,31 @@
+public class pattern {
+  public static void main(String[] args) {
+    //  int n = 5;
+        
+    //     for(int i = 0; i < n; i++) {
+    //         for(int j = 0; j < 2 * n; j++) {
+    //           if(j <= i || j >= 2 * n - i - 1) System.out.print("* ");
+    //           else System.out.print("  "); 
+    //         }
+    //         System.out.println();
+    //     }
+    //     for(int i = 0; i < n; i++) {
+    //       for(int j = 0; j < 2 * n; j++) {
+    //         if(j < n - i || j >= n + i) System.out.print("* ");
+    //         else System.out.print("  ");
+            
+    //       }
+    //       System.out.println();
+    //   }
+      // int n = 4;
+        
+      //   for(int i = 1; i <= n; i++) {
+      //       for(int j = n; j >= 1; j--) {
+      //           if(j <= i) System.out.print(j + " ");
+      //           else System.out.print("  ");
+      //       }
+      //       System.out.println();
+      //   }
+
+  }
+}
